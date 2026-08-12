09ba5dc265818ca3e16217943811be84728438f4
vidhya <vidhyamani24032007@gmail.com>
2026-08-12 16:27:44 +0530

    Basic Program

--- Factorial.java
+++ Factorial.java
@@ -28,3 +28,22 @@ public class Factorial {
     }
 }
 }
+
+/*public class Factorial {
+
+    static long factorial(int n) {
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        System.out.println("Factorial = " + factorial(n));
+    }
+} */
--- FibonacciSum.java
+++ FibonacciSum.java
@@ -28,3 +28,12 @@ public class FibonacciSum {
     }
 }
 }
+
+/* static int fibonacci(int n) {
+
+    if (n <= 1) {
+        return n;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}*/
